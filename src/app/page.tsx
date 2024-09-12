07c23cb746722247083e5371f8940b16a28965bf
Shubham Patil <122681369+ShubhamPatilSp@users.noreply.github.com>
2024-09-12 19:20:54 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Footer from "./componets/footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <HeroSection />
+      <HeroSection /> 
       <FeaturedCourses />
       {/* <WhyChooseUs/> */}
       {/* <MusicSchoolTestTestimonial/> */}
