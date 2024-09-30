da6a474ae2f4dcb44eb55c79ef260815d3429a51
Shubham Patil <shubhamkpatil474@gmail.com>
2024-09-30 10:04:35 +0530

    add

--- src/app/componets/Projects.tsx
+++ src/app/componets/Projects.tsx
@@ -58,7 +58,7 @@ function Projects() {
                                             as="button"
                                             className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                                         >
-                                            Live
+                                            See Me
                                         </CardItem>
                                     </Link>
                                 )}
