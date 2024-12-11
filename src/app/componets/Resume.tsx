8c268034d08c4908b7973b8da39a497b99a42a9d
Shubham Patil <122681369+ShubhamPatilSp@users.noreply.github.com>
2024-12-11 18:32:30 +0530

    reume update

--- src/app/componets/Resume.tsx
+++ src/app/componets/Resume.tsx
@@ -22,14 +22,13 @@ const Resume = () => {
             <div className="max-w-5xl mx-auto bg-gray-900 shadow-2xl rounded-lg overflow-hidden border border-gray-800">
                 <header className="bg-gradient-to-r from-purple-900 to-indigo-900 text-white p-10">
                     <h1 className="text-5xl font-bold mb-2">Shubham Patil</h1>
-                    <p className="text-xl mb-4">Full Stack Developer</p>
                     <div className="flex flex-wrap gap-4 text-sm">
                         {[
                             { name: "xshuubhampatil@gmail.com", link: "mailto:xshuubhampatil@gmail.com" },
                             { name: "+91 8421518624", link: "tel:+918421518624" },
                             { name: "LinkedIn", link: "https://www.linkedin.com/in/shubham-patil-8494982b0" },
                             { name: "GitHub", link: "https://github.com/ShubhamPatilSp" },
-                            { name: "Portfolio", link: "#" }
+                            { name: "X", link: "https://x.com/x__shubham" }
                         ].map((item, index) => (
                             <a
                                 key={index}
@@ -46,18 +45,56 @@ const Resume = () => {
 
                 <main className="p-10">
                     <section className="mb-12">
-                        <h2 className="text-3xl font-semibold mb-6 text-white">Projects</h2>
+                        <h2 className="text-3xl font-semibold mb-6 text-white">Freelancer Projects</h2>
                         {isClient && (
                             <>
                                 <ProjectCard
+                                    title="SERP Live"
+                                    description="Developed a web app for location-based searches with real-time filtering using Next.js and TypeScript."
+                                    details={[
+                                        "Real-time search functionality",
+                                        "Location suggestions with auto-updating results",
+                                        "Responsive and interactive user interface",
+                                    ]}
+                                    technologies={[
+                                        "Next.js",
+                                        "Tailwind CSS", 
+                                        "TypeScript", 
+                                        "SerpAPI", 
+                                        "Axios", 
+                                        "Vercel"
+                                    ]}
+                                />
+                                <ProjectCard
+                                    title="AI Chat Assistant"
+                                    description="Built a real-time chat application powered by Google's Gemini AI, enabling instant AI interactions with a modern user interface."
+                                    details={[
+                                        "Real-time chat functionality",
+                                        "Clean session management",
+                                        "Responsive design",
+                                        "Elegant error handling",
+                                    ]}
+                                    technologies={[
+                                        "Next.js 14",
+                                        "TypeScript", 
+                                        "Tailwind CSS", 
+                                        "Google Generative AI", 
+                                        "LibSQL", 
+                                        "React Hooks"
+                                    ]}
+                                />
+                        <h2 className="text-3xl font-semibold mb-6 text-white">Projects</h2>
+
+                                <ProjectCard
+                                
                                     title="AutoDeploy Pipeline"
                                     description="Developed a comprehensive automation pipeline to clone repositories, build projects, and upload outputs to AWS S3 using Node.js and Redis for logging."
                                     details={[
-                                        "Repository Cloning: Utilized child process to clone Git repositories and execute build scripts.",
-                                        "Build Process: Managed project builds by reading package.json, executing npm commands, and handling stdout/stderr logging.",
-                                        "S3 Integration: Employed the AWS SDK to upload built files to a specified S3 bucket.",
-                                        "Logging Mechanism: Integrated Redis for real-time logging of the pipeline process.",
-                                        "Environment Configuration: Utilized environment variables for secure configuration.",
+                                        "Repository Cloning: Utilized child process to clone Git repositories and execute build scripts",
+                                        "Build Process: Managed project builds by reading package.json, executing npm commands",
+                                        "S3 Integration: Employed the AWS SDK to upload built files to a specified S3 bucket",
+                                        "Logging Mechanism: Integrated Redis for real-time logging of the pipeline process",
+                                        "Environment Configuration: Utilized environment variables for secure configuration",
                                     ]}
                                     technologies={[
                                         "Node.js",
@@ -76,11 +113,11 @@ const Resume = () => {
                                     title="Video Transcoder"
                                     description="Automated video transcoding pipeline using AWS S3 and FFmpeg for multiple resolutions."
                                     details={[
-                                        "S3 Integration: Utilizes the AWS SDK to download original videos from S3 and upload transcoded versions.",
-                                        "FFmpeg Utilization: Implements fluent-ffmpeg to transcode videos into 360p, 480p, and 720p resolutions.",
-                                        "Environment Management: Loads environment variables for configuration.",
-                                        "Asynchronous Processing: Employs Promises for handling multiple transcoding operations concurrently.",
-                                        "File Handling: Utilizes Node.js's fs module for file operations.",
+                                        "S3 Integration: Utilizes the AWS SDK to download original videos from S3 and upload transcoded versions",
+                                        "FFmpeg Utilization: Implements fluent-ffmpeg to transcode videos into 360p, 480p, and 720p resolutions",
+                                        "Environment Management: Loads environment variables for configuration",
+                                        "Asynchronous Processing: Employs Promises for handling multiple transcoding operations concurrently",
+                                        "File Handling: Utilizes Node.js's fs module for file operations",
                                     ]}
                                     technologies={[
                                         "Node.js",
@@ -95,34 +132,37 @@ const Resume = () => {
                                 />
                                 <ProjectCard
                                     title="Traveller"
-                                    description="A refined travel platform inspired by Airbnb, offering personalized stays."
+                                    description="A travel platform inspired by Airbnb, offering personalized stays with secure authentication and responsive design."
                                     details={[
-                                        "Dynamic User Authentication: Integrates Passport.js for seamless user authentication and session management.",
-                                        "Enhanced Data Handling: Utilizes Express.js and Joi for dynamic routing and robust data validation.",
-                                        "Image Management: Integrates Cloudinary for efficient image storage and management.",
-                                        "User Experience: Features responsive design with Bootstrap and EJS.",
-                                        "Comprehensive Middleware: Implements middleware for session management, error handling, and validation.",
+                                        "User authentication using Passport.js and Express-Session",
+                                        "Data validation with Joi",
+                                        "Image management via Cloudinary",
+                                        "Responsive and user-friendly design",
                                     ]}
                                     technologies={[
                                         "MERN Stack",
-                                        "EJS",
-                                        "Passport.js",
-                                        "Express-Session",
-                                        "Joi",
-                                        "Cloudinary",
-                                        "Bootstrap",
+                                        "Passport.js", 
+                                        "Joi", 
+                                        "Cloudinary", 
+                                        "Bootstrap"
                                     ]}
                                 />
                                 <ProjectCard
-                                    title="Portfolio"
-                                    description="A modern portfolio website built with Next.js and Tailwind CSS, showcasing projects and skills with a visually striking design."
+                                    title="Photography Studio"
+                                    description="A sleek platform showcasing photography with immersive animations and an interactive user experience."
                                     details={[
-                                        "Design Features: Incorporates animated tooltips, gradient backgrounds, and hover effects.",
-                                        "Interactive Elements: Includes infinite scrolling, custom navigation menus, spotlight effects, and sticky scroll reveals.",
-                                        "Showcase Sections: Highlights featured projects, includes a hero section, team member section, and custom footer.",
-                                        "Custom Components: Features testimonial cards and a section detailing reasons to choose the services offered.",
+                                        "Smooth scrolling implementation",
+                                        "Responsive and interactive design",
+                                        "Optimized image loading",
+                                        "Engaging user interface",
                                     ]}
-                                    technologies={["Next.js", "React.js", "Tailwind CSS", "JavaScript", "TypeScript"]}
+                                    technologies={[
+                                        "Next.js", 
+                                        "React", 
+                                        "Tailwind CSS", 
+                                        "Framer Motion", 
+                                        "Lenis Scroll"
+                                    ]}
                                 />
                             </>
                         )}
@@ -151,47 +191,26 @@ const Resume = () => {
                                     "Express.js",
                                     "Mongoose",
                                     "MongoDB",
-                                    "Redis",
-                                    "Kafka",
-                                    "Postgresql",
-                                    "Prisma",
                                 ]}
                             />
                             <SkillCategory
-                                title="DevOps"
+                                title="Basic DevOps"
                                 skills={[
                                     "Docker",
                                     "AWS",
-                                    "Kubernetes",
                                     "CI/CD",
-                                ]}
-                            />
-                            <SkillCategory
-                                title="Other Skills"
-                                skills={[
-                                    "Monorepos (Turborepo)",
-                                    "OpenAPI",
-                                    "Message Queues (Kafka, Redis)",
-                                    "Testing (Node.js applications)",
-                                    "Rate Limiting",
-                                    "Authentication (NextAuth, Passport.js)",
                                 ]}
                             />
                         </div>
                     </section>
 
-                    <section>
+                    <section className="mb-12">
                         <h2 className="text-3xl font-semibold mb-6 text-white">Education</h2>
                         <div className="bg-gray-800 p-6 rounded-lg">
-                            <h3 className="text-2xl font-medium text-white mb-2">
-                                Bachelor of Computer Application
-                            </h3>
-                            <p className="text-lg mb-1">Pratap College, Amalner, India</p>
-                            <p className="text-gray-400 mb-4">July 2020 to July 2023</p>
-                            <p>
-                                Relevant Coursework: Database management, web development,
-                                computer networks, software engineering.
-                            </p>
+                            <h3 className="text-xl font-semibold text-white mb-2">Bachelor of Computer Application</h3>
+                            <p className="text-gray-300">Kavayitri Bahinabai Chaudhari North Maharashtra University</p>
+                            <p className="text-gray-400">July 2019 – July 2023</p>
+                            <p className="text-gray-400">Amalner, India</p>
                         </div>
                     </section>
                 </main>
@@ -254,3 +273,4 @@ const SkillCategory = ({ title, skills }) => (
 );
 
 export default Resume;
+
